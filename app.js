21f0033b7d431f65f7bca137b3e84309c3189092
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-23 20:19:29 -0600

    Add basic commands of notes app

---
+++ app.js
@@ -0,0 +1,53 @@
+const note = require('./notes')
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+yargs.version('1.1.0')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    note.addNote(argv.title, argv.body)
+  }
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Removing the note')
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'Lists the notes',
+  handler: () => {
+    console.log('Listing notes')
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Reads the notes',
+  handler: () => {
+    console.log('Reading the notes')
+  }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
